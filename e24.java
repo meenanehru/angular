bfdd48b2f7217fd0250dd5c019ba0f13876de8ea
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-09-27 18:31:03 +0530

    Create e24.java

---
+++ e24.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+class e24
+{
+	public static void main(String args[])
+	{
+		int n,i,a1,j=0;
+		Scanner ob = new Scanner(System.in);
+		System.out.println("Enter the N value :");
+		n=ob.nextInt();
+		int a[] = new int[n];
+		for(i=0;i<n;i++)
+		{
+		    a[i] = ob.nextInt();
+		
+		}
+        for(i=0;i<n;i++)
+        {			
+	       for(j=i+1;j<n;j++)
+		   {   
+		     if(a[i]>a[j])
+			 {
+				 a1 =a[i];
+			     a[i] = a[j];
+				 a[j] = a1; 
+			 }
+		   }
+			
+		}
+		
+		for(i=0;i<n;i++)
+		{
+		    System.out.println(a[i]);
+		}
+	}
+}	
