3324e6acd117e9c2c9dc70b2eef6f8943a5c406a
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-09-27 11:58:28 +0530

    Create e14.java

---
+++ e14.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+class e14
+{
+	public static void main(String args[])
+	{
+		 int n,q;
+		 Scanner ob =new Scanner(System.in);
+		 System.out.println("Enter the N value :");
+		 n=ob.nextInt();
+		 System.out.println("Enter the Q value :");
+		 q=ob.nextInt();
+		 while(n<q)
+		 {
+		    n++;
+			//System.out.println("n="+n);
+		    if(n%2!=0) 
+			{
+		       if(q==n)
+			   {
+				  System.exit(0);
+			   }
+			   else
+			   {  
+				     System.out.print(n+" ");
+			   }
+			}
+			
+		}
+	}
+}
