c9d6d0896e4586366ca53fd56fc74ee1727eab33
meenanehru <36810964+meenanehru@users.noreply.github.com>
2018-10-10 19:19:40 +0530

    Create p53.java

---
+++ p53.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+class pn
+{
+    void cal(String a1)
+    {
+	  int len,i,j=0,k=97,k1=65;
+	  int as[] = new int[100];
+	  len = a1.length();
+	  for(i=0;i<len;i++)
+	  {
+		  as[i]=a1.charAt(i);
+		  if((k==as[i] && k<=122) || (k1==as[i] && k1<=90))
+		  { 
+			 j++;
+			 if(j==26)
+			 {
+				System.out.println("Yes");
+				System.exit(0);
+			 }
+			 k++;k1++;
+             i=-1;
+          }  
+		  else
+		  {
+			  System.out.println("No");
+			  System.exit(0);
+		  }
+	  }
+	  
+	}
+}	    
+class p53
+{
+            public static void main(String args[])
+	        {
+		                String a;
+		                Scanner ob = new Scanner(System.in);
+		                System.out.println("Enter the String :\t");
+						a = ob.nextLine();
+						pn o=new pn();
+						o.cal(a);		
+	        }
+}
+			
